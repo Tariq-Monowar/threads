dc771f08126932fcd1b18408acf0fe805c7dad5e
Tariq-Monowar <hassainseam@gmail.com>
2025-10-07 09:47:09 +0600

    first commit

---
+++ src/routes/v1/auth/auth.controllers.ts
@@ -0,0 +1,26 @@
+import { FastifyRequest, FastifyReply } from "fastify";
+import bcrypt from "bcrypt";
+import {
+  forgotPasswordEmail,
+  otpVerificationEmail,
+  sendForgotPasswordOTP,
+  sendTwoFactorOtp,
+} from "../../../utils/email.config";
+
+import { generateJwtToken } from "../../../utils/jwt.utils";
+import { getImageUrl } from "../../../utils/baseurl";
+import path from "path";
+import fs from "fs";
+import { v4 as uuidv4 } from "uuid";
+import { authenticator } from "otplib";
+import { uploadsDir } from "../../../config/storage.config";
+
+export const test = async (request, reply) => {
+  try {
+  } catch (error) {
+    request.log.error(error);
+    return reply
+      .status(500)
+      .send({ succes: false, error: error, message: "Internal Server Error" });
+  }
+};

4e59319e08d74a257471e1f12e4d5204d614e2ec
Tariq Monowar <hassainseam@gmail.com>
2025-10-17 12:53:26 +0600

    message room

--- src/routes/v1/messages/messages.routes.ts
+++ src/routes/v1/messages/messages.routes.ts
@@ -1,10 +1,10 @@
 import { FastifyInstance } from "fastify";
- 
+import { createChatRoom } from "./messages.controllers";
 import { upload } from "../../../config/storage.config";
 import { verifyUser } from "../../../middleware/auth.middleware";
 
 const authRoutes = (fastify: FastifyInstance) => {
- 
+  fastify.post("/create-chat-room", createChatRoom);
 
 };
 

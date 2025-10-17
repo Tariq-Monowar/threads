0c241486d7fa44f2397e0a5e0b67d932d2ddaddb
Tariq Monowar <hassainseam@gmail.com>
2025-10-17 09:28:37 +0600

    messages

---
+++ src/routes/v1/messages/messages.routes.ts
@@ -0,0 +1,11 @@
+import { FastifyInstance } from "fastify";
+ 
+import { upload } from "../../../config/storage.config";
+import { verifyUser } from "../../../middleware/auth.middleware";
+
+const authRoutes = (fastify: FastifyInstance) => {
+ 
+
+};
+
+export default authRoutes;
